c99de8ba152d9ba24588721eb5c7fa1926a93f7f
shnvandiepen1 <s.vandiepen@student.maastrichtuniversity.nl>
2021-01-05 22:59:22 +0100

    added piece file

---
+++ Piece.java
@@ -0,0 +1,17 @@
+/*
+ *  int k in constructor stands for the type of piece that is created and goes from 0-5
+ *  0 = pawn
+ *  1 = knight
+ *  2 = bishop
+ *  3 = rook
+ *  4 = queen
+ *  5 = king
+ */
+public class Piece {
+    private int type;
+
+    public Piece(int k){
+        
+    }
+    public int getType() { return type; }
+}
